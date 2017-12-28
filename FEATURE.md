7b9db982dc3d5382970be52f997ddef8cdc7b100
zouzhangwei <zouzhangwei@hujiang.com>
2017-12-28 17:55:01 +0800

    update tarball file, to support brew install

--- FEATURE.md
+++ FEATURE.md
@@ -1,3 +1,4 @@
 ### Feature
 1. 增肌template自定义方式，提过template参数返回模板名称。
 2. 增加koi命令有homebrew安装
+3. 增加创建后项目使用的语言
--- Formula/koi.rb
+++
@@ -1,9 +0,0 @@
-class Koi < Formula
-  homepage ''
-  desc 'Koi who a gradle plugin creator fast.'
-  version '1.0.0'
-
-  test do
-  system /bin/"bash", "-c", "printf -v hello -- '%s'"
-  end
-end
